53d0d38a2f3c344c78576790a5dd2f1b539304de
ryo-yama-dev <springwindwriter@gmail.com>
2023-09-17 11:09:06 +0900

    feat: client-presetの導入

---
+++ codegen.ts
@@ -0,0 +1,20 @@
+import { CodegenConfig } from "@graphql-codegen/cli"
+
+const config: CodegenConfig = {
+  schema: "http://127.0.0.1:8080/graphql",
+  documents: ["src/**/*.tsx", "src/**/*.ts"],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    "./src/lib/graphql/": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      preset: "client",
+    },
+  },
+  hooks: { afterAllFileWrite: ["prettier --write ."] },
+}
+
+export default config
