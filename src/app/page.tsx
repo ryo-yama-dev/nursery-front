caf953042bb49e96f78b06fda62e860cc9a0851b
ryo-yama-dev <springwindwriter@gmail.com>
2023-12-24 17:25:34 +0900

    feat: トップページ更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,46 @@
-import { Button } from "components/common"
 import Link from "next/link"
+import clsx from "clsx"
+
+const TopButton = ({
+  color,
+  children,
+}: {
+  color: string
+  children: React.ReactNode
+}) => {
+  return (
+    <button
+      className={clsx(
+        "h-80 w-80 rounded-md border border-teal-500 text-3xl ",
+        color,
+      )}
+    >
+      {children}
+    </button>
+  )
+}
 
 export default function TopPage() {
   return (
-    <div className="flex h-full w-full items-center justify-between">
-      <Link href="/employee">
-        <Button>勤怠登録</Button>
-      </Link>
-      <Link href="/child">
-        <Button>登退園登録</Button>
-      </Link>
-      <Link href="/manage">
-        <Button>従業員管理</Button>
-      </Link>
+    <div className="h-full w-full">
+      <div className="my-2 flex items-center justify-center p-2">
+        <p className="py-4 text-4xl">保育園情報管理システム</p>
+      </div>
+      <div className="mt-4 flex items-center justify-evenly">
+        <Link href="/employee">
+          <TopButton color="bg-cyan-100 hover:bg-cyan-300">勤怠登録</TopButton>
+        </Link>
+        <Link href="/child">
+          <TopButton color="bg-lime-100 hover:bg-lime-300">
+            登退園登録
+          </TopButton>
+        </Link>
+        <Link href="/manage">
+          <TopButton color="bg-amber-100 hover:bg-amber-300">
+            従業員管理
+          </TopButton>
+        </Link>
+      </div>
     </div>
   )
 }
